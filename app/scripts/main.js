cf9e8ae062dc427efa0e901254a982cdc64f20f8
Andres V <darklink898@gmail.com>
2014-02-19 00:51:13 -0600

    recetas wip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
 console.log('\'Allo \'Allo!');
+
+$(function () {
+    for (var i = 0; i < 2; i++) {
+        var $lastRow = $('#ingredientes-receta .fila-ingrediente:last-child')
+        $lastRow.clone().insertAfter($lastRow);
+    }
+
+    $('#ingredientes-receta').on('keyup', '.fila-ingrediente:last-child input', function () {
+        var $lastRow = $('#ingredientes-receta .fila-ingrediente:last-child'),
+            $newRow,
+            valid;
+
+        valid = $lastRow.find('input').filter(function () {
+            return $(this).val();
+        }).length == 2;
+
+        if (valid) {
+            $newRow = $lastRow.clone();
+            $newRow.find('input').val('');
+            $newRow.insertAfter($lastRow);
+        }
+    });
+});
