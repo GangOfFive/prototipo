e97fb7067073bb5d3d56d1c6ff193b2c5f75f080
Leo Picado <leopic@gmail.com>
2014-02-25 21:45:22 -0600

    Agregando la dependencia del calendario a bower, removiendo JS.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -68,13 +68,13 @@ $(function () {
             defaultDate: '3/3/2014',
             pickTime: false
         }).on('change.dp', function() {
-            var fecha = $(this).data('DateTimePicker').getDate();
+                var fecha = $(this).data('DateTimePicker').getDate();
 
-            if ($('#fecha').val()) {
-                $('#citas-para-fecha').slideDown();
-                $('#fechaSeleccionada').text(fecha.format('DD [de] MMMM [del] YYYY'));
-            }
-        });
+                if ($('#fecha').val()) {
+                    $('#citas-para-fecha').slideDown();
+                    $('#fechaSeleccionada').text(fecha.format('DD [de] MMMM [del] YYYY'));
+                }
+            });
 
         $('#datetimepicker2').datetimepicker({
             useSeconds: false,
@@ -97,4 +97,56 @@ $(function () {
     }
 
     $('#inyectar-pacientes').load('partials/lista-pacientes.html');
+
+    // Calendario
+    if (('#calendar').length) {
+        var calendarOpts = {
+            events_source: '/scripts/citas.json',
+            view: 'month',
+            tmpl_path: '/bower_components/bootstrap-calendar/tmpls/',
+            tmpl_cache: false,
+            day: '2014-03-12',
+            language: 'es-ES',
+            onAfterEventsLoad: function(events) {
+                if(!events) {
+                    return;
+                }
+                var list = $('#eventlist');
+                list.html('');
+
+                $.each(events, function(key, val) {
+                    $(document.createElement('li'))
+                        .html('<a href="' + val.url + '">' + val.title + '</a>')
+                        .appendTo(list);
+                });
+            },
+            onAfterViewLoad: function(view) {
+                $('.page-header h3').text(this.getTitle());
+                $('.btn-group button').removeClass('active');
+                $('button[data-calendar-view="' + view + '"]').addClass('active');
+            },
+            classes: {
+                months: {
+                    general: 'label'
+                }
+            }
+        };
+
+        var calendar = $('#calendar').calendar(calendarOpts);
+
+        $('.btn-group button[data-calendar-nav]').each(function() {
+            var $this = $(this);
+            $this.click(function() {
+                calendar.navigate($this.data('calendar-nav'));
+            });
+        });
+
+        $('.btn-group button[data-calendar-view]').each(function() {
+            var $this = $(this);
+            $this.click(function() {
+                calendar.view($this.data('calendar-view'));
+            });
+        });
+    }
+
 });
