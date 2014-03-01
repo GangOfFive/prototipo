0b5957517fc61e9db09d60f140536ec5fd414edb
haden9 <haden9@gmail.com>
2014-02-28 18:17:29 -0600

    Ver calendario, calendarizar cita

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -119,6 +119,7 @@ $(function () {
 
     // Calendario
     if ($('#calendar').length) {
+        console.log('hello world1');
         var calendarOpts = {
             events_source: '/scripts/citas.json',
             view: 'month',
@@ -152,6 +153,57 @@ $(function () {
         };
 
         var calendar = $('#calendar').calendar(calendarOpts);
+
+        $('.btn-group button[data-calendar-nav]').each(function() {
+            var $this = $(this);
+            $this.click(function() {
+                calendar.navigate($this.data('calendar-nav'));
+            });
+        });
+
+        $('.btn-group button[data-calendar-view]').each(function() {
+            var $this = $(this);
+            $this.click(function() {
+                calendar.view($this.data('calendar-view'));
+            });
+        });
+    }
+
+    if ($('#calendar2').length) {
+        console.log('hello world');
+        var calendarOpts = {
+            events_source: '/scripts/citas2.json',
+            view: 'month',
+            tmpl_path: '/bower_components/bootstrap-calendar/tmpls/',
+            tmpl_cache: false,
+            day: '2014-03-12',
+            language: 'es-ES',
+            onAfterEventsLoad: function(events) {
+                if(!events) {
+                    return;
+                }
+                var list = $('#eventlist');
+                list.html('');
+
+                $.each(events, function(key, val) {
+                    $(document.createElement('li'))
+                        .html('<a href="' + val.url + '">' + val.title + '</a>')
+                        .appendTo(list);
+                });
+            },
+            onAfterViewLoad: function(view) {
+                $('.page-header h3').text(this.getTitle());
+                $('.btn-group button').removeClass('active');
+                $('button[data-calendar-view="' + view + '"]').addClass('active');
+            },
+            classes: {
+                months: {
+                    general: 'label'
+                }
+            }
+        };
+
+        var calendar = $('#calendar2').calendar(calendarOpts);
 
         $('.btn-group button[data-calendar-nav]').each(function() {
             var $this = $(this);
